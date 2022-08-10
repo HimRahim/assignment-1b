1331c64864b1e3635053434a71ae667b7a438444
HimRahim <6210110690@psu.ac.th>
2022-08-10 16:08:35 +0700

    assignment 1b

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,19 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: '@directus/gatsby-source-directus',
+      options: {
+        url: `https://g3yw4g18.directus.app`, // Fill with your Directus instance address
+        auth: {
+          //token: 'my_secret_token', // You can use a static token from an user
+
+          // Or you can use the credentials of an user
+          email: 'jackside555@gmail.com',
+          password: 'PJuIPYU8gSX_-W[0lxrbaH]p',
+        },
+      },
+    },
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,176 +1,234 @@
-import * as React from "react"
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import '../styles/styles.css';
+import {
+  Box,
+  Button,
+  Link,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
+import { tableCellClasses } from '@mui/material/TableCell';
+import { styled } from '@mui/material/styles';
+import { useStaticQuery, graphql } from 'gatsby';
+import Navbar from '../components/Navbar';
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
   },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
   },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
   },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
   },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+}));
+
+const imageLink = 'https://g3yw4g18.directus.app/assets/';
 
 const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      directus {
+        students {
+          student_id
+          student_name
+          student_image {
+            filename_disk
+          }
+          registrations {
+            registrations_id {
+              course_id
+              course_name
+              day
+              schedule
+            }
+          }
+        }
+      }
+    }
+  `);
+  console.log(data.directus.students[0]);
+
+  const [stdId, setStdId] = React.useState('');
+  const [stdName, setStdName] = React.useState('');
+  const [selectedStd, setSelectedStd] = React.useState(null);
+  const [renderProfile, setRenderProfile] = React.useState(false);
+  const [renderTable, setRenderTable] = React.useState(true);
+
+  const handleSubmit = () => {
+    if (stdName.length === 0 || stdId.length === 0) return;
+    console.log(stdName);
+  };
+
+  console.log(selectedStd);
+
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+    <>
+      <div>
+        <Navbar />
+      </div>
+      {/* <div className="container">
+        <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1, width: '50ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="std-id"
+            label="Student ID"
+            variant="outlined"
+            onChange={(e) => setStdId(e.target.value)}
+          />
+          <br />
+          <TextField
+            id="std-name"
+            label="Student Name"
+            variant="outlined"
+            onChange={(e) => setStdName(e.target.value)}
+          />
+          <br />
+        </Box>
+        <div className="btn-group">
+          <Button variant="contained" onClick={handleSubmit}>
+            Search
+          </Button>
+        </div>
+      </div> */}
+
+      {renderTable && (
+        <div className="table">
+          {/* <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Student ID</TableCell>
+              <TableCell align="center">Student Name</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.directus.students.map((student) => (
+              <TableRow
+                key={student.student_id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+                <TableCell component="th" scope="row">
+                  {student.student_id}
+                </TableCell>
+                <TableCell align="center">{student.student_name}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer> */}
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell>Student ID</StyledTableCell>
+                  <StyledTableCell align="left">Student Name</StyledTableCell>
+                  <StyledTableCell align="left">Information</StyledTableCell>
+                  {/* <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
+            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell> */}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.directus.students.map((student) => (
+                  <StyledTableRow key={student.student_id}>
+                    <StyledTableCell component="th" scope="row">
+                      {student.student_id}
+                    </StyledTableCell>
+                    <StyledTableCell align="left">
+                      {student.student_name}
+                    </StyledTableCell>
+                    <StyledTableCell align="left">
+                      <Button
+                        onClick={() => {
+                          setSelectedStd(student);
+                          setRenderTable(false);
+                          setRenderProfile(true);
+                        }}
+                      >
+                        See more
+                      </Button>
+                    </StyledTableCell>
+                    {/* <StyledTableCell align="right">
+                    {student.student_id}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">
+                    {student.student_id}
+                  </StyledTableCell> */}
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+      )}
 
-export default IndexPage
+      {renderProfile && (
+        <div className="profile-container">
+          <div className="img-container">
+            <img
+              src={`${imageLink}${selectedStd.student_image.filename_disk}`}
+              alt="student image"
+              height={300}
+            />
+          </div>
+          <div className="type-container">
+            <Box sx={{ width: '100%', maxWidth: 500, mt: '60px', ml: '50px' }}>
+              <Typography variant="h5" component="div" gutterBottom>
+                Student ID: {selectedStd.student_id}
+              </Typography>
+              <Typography variant="h5" component="div" gutterBottom>
+                Student Name: {selectedStd.student_name}
+              </Typography>
+              <Typography variant="h5" component="div" gutterBottom>
+                Courses:
+              </Typography>
+              {selectedStd.registrations.map((course) => {
+                return (
+                  <Typography key={course.registrations_id.course_id} variant="h5" component="div" gutterBottom>
+                    {course.registrations_id.course_id}{' '}
+                    {course.registrations_id.course_name}
+                  </Typography>
+                );
+              })}
+            </Box>
+            <Button
+              sx={{ ml: '250px', mt: '50px' }}
+              variant="contained"
+              color="error"
+              onClick={() => {
+                setRenderProfile(false)
+                setRenderTable(true)
+              }}
+            >
+              Back
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
-export const Head = () => <title>Home Page</title>
+export default IndexPage;
