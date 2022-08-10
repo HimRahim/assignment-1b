89701659d19c59b6bd7e08363a47742c161000ae
HimRahim <6210110690@psu.ac.th>
2022-08-10 18:45:06 +0700

    last edit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import * as React from 'react';
-import TextField from '@mui/material/TextField';
 import '../styles/styles.css';
 import {
   Box,
-  Button,
   Link,
   Paper,
   Table,
@@ -63,170 +61,81 @@ const IndexPage = () => {
       }
     }
   `);
-  console.log(data.directus.students[0]);
-
-  const [stdId, setStdId] = React.useState('');
-  const [stdName, setStdName] = React.useState('');
-  const [selectedStd, setSelectedStd] = React.useState(null);
-  const [renderProfile, setRenderProfile] = React.useState(false);
-  const [renderTable, setRenderTable] = React.useState(true);
-
-  const handleSubmit = () => {
-    if (stdName.length === 0 || stdId.length === 0) return;
-    console.log(stdName);
-  };
-
-  console.log(selectedStd);
 
   return (
     <>
       <div>
         <Navbar />
       </div>
-      {/* <div className="container">
-        <Box
-          component="form"
-          sx={{
-            '& > :not(style)': { m: 1, width: '50ch' },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            id="std-id"
-            label="Student ID"
-            variant="outlined"
-            onChange={(e) => setStdId(e.target.value)}
-          />
-          <br />
-          <TextField
-            id="std-name"
-            label="Student Name"
-            variant="outlined"
-            onChange={(e) => setStdName(e.target.value)}
-          />
-          <br />
-        </Box>
-        <div className="btn-group">
-          <Button variant="contained" onClick={handleSubmit}>
-            Search
-          </Button>
-        </div>
-      </div> */}
 
-      {renderTable && (
-        <div className="table">
-          {/* <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Student ID</TableCell>
-              <TableCell align="center">Student Name</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.directus.students.map((student) => (
-              <TableRow
-                key={student.student_id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {student.student_id}
-                </TableCell>
-                <TableCell align="center">{student.student_name}</TableCell>
+      <div className="table">
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 700 }} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Student ID</StyledTableCell>
+                <StyledTableCell align="left">Student Name</StyledTableCell>
+                <StyledTableCell align="left">Information</StyledTableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer> */}
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 700 }} aria-label="customized table">
-              <TableHead>
-                <TableRow>
-                  <StyledTableCell>Student ID</StyledTableCell>
-                  <StyledTableCell align="left">Student Name</StyledTableCell>
-                  <StyledTableCell align="left">Information</StyledTableCell>
-                  {/* <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell> */}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {data.directus.students.map((student) => (
-                  <StyledTableRow key={student.student_id}>
-                    <StyledTableCell component="th" scope="row">
-                      {student.student_id}
-                    </StyledTableCell>
-                    <StyledTableCell align="left">
-                      {student.student_name}
-                    </StyledTableCell>
-                    <StyledTableCell align="left">
-                      <Button
-                        onClick={() => {
-                          setSelectedStd(student);
-                          setRenderTable(false);
-                          setRenderProfile(true);
-                        }}
-                      >
-                        See more
-                      </Button>
-                    </StyledTableCell>
-                    {/* <StyledTableCell align="right">
+            </TableHead>
+            <TableBody>
+              {data.directus.students.map((student) => (
+                <StyledTableRow key={student.student_id}>
+                  <StyledTableCell component="th" scope="row">
                     {student.student_id}
                   </StyledTableCell>
-                  <StyledTableCell align="right">
-                    {student.student_id}
-                  </StyledTableCell> */}
-                  </StyledTableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </div>
-      )}
+                  <StyledTableCell align="left">
+                    {student.student_name}
+                  </StyledTableCell>
+                  <StyledTableCell align="left">
+                    <Link underline="none" href={`#${student.student_id}`}>
+                      See more
+                    </Link>
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
 
-      {renderProfile && (
-        <div className="profile-container">
+      {data.directus.students.map((student) => (
+        <div className="profile-container" id={student.student_id}>
           <div className="img-container">
             <img
-              src={`${imageLink}${selectedStd.student_image.filename_disk}`}
-              alt="student image"
+              src={`${imageLink}${student.student_image.filename_disk}`}
+              alt="student"
               height={300}
             />
           </div>
-          <div className="type-container">
+          <div className="typo-container">
             <Box sx={{ width: '100%', maxWidth: 500, mt: '60px', ml: '50px' }}>
               <Typography variant="h5" component="div" gutterBottom>
-                Student ID: {selectedStd.student_id}
+                Student ID: {student.student_id}
               </Typography>
               <Typography variant="h5" component="div" gutterBottom>
-                Student Name: {selectedStd.student_name}
+                Student Name: {student.student_name}
               </Typography>
               <Typography variant="h5" component="div" gutterBottom>
                 Courses:
               </Typography>
-              {selectedStd.registrations.map((course) => {
+              {student.registrations.map((course) => {
                 return (
-                  <Typography key={course.registrations_id.course_id} variant="h5" component="div" gutterBottom>
+                  <Typography
+                    key={course.registrations_id.course_id}
+                    variant="h5"
+                    component="div"
+                    gutterBottom
+                  >
                     {course.registrations_id.course_id}{' '}
                     {course.registrations_id.course_name}
                   </Typography>
                 );
               })}
             </Box>
-            <Button
-              sx={{ ml: '250px', mt: '50px' }}
-              variant="contained"
-              color="error"
-              onClick={() => {
-                setRenderProfile(false)
-                setRenderTable(true)
-              }}
-            >
-              Back
-            </Button>
           </div>
         </div>
-      )}
+      ))}
     </>
   );
 };
